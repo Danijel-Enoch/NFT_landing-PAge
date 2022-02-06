4469e3a3526012958aad054fc552fda379f6210f
Danijel-Enoch <enochdanijel@gmail.com>
2022-02-06 08:54:53 +0100

    adapter Commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: staticAdapter(),
 	},
 
 	preprocess: [
